37844b1e6477aae904252c509e42eb13d2e6ae2d
Fernando Toledo <fernandohtr@gmail.com>
2024-08-04 09:09:57 -0300

    chore: use argon2 as password hasher

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -52,6 +52,13 @@ DATABASES = {
         "NAME": ROOT_DIR / "db.sqlite3",
     }
 }
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.Argon2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
+    "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
+    "django.contrib.auth.hashers.ScryptPasswordHasher",
+]
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
