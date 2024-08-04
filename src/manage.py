6abc57adaa0278e925aedffde2a1c400e2a836e0
Fernando Toledo <fernandohtr@gmail.com>
2024-08-04 09:42:08 -0300

    chore: set settings.local as default

--- src/manage.py
+++ src/manage.py
@@ -7,7 +7,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+    # TODO change this in production
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
