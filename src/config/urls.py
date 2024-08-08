b16173a393970d920aa057b37226789c98b05cb5
Fernando Toledo <fernandohtr@gmail.com>
2024-08-07 21:11:16 -0300

    configure drfspectacular

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,7 +1,14 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+
+DRF_SPECTACULAR_URLS = [
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/schema/swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger"),
+    path("api/schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+]
 
 urlpatterns = [
     path(settings.ADMIN_URL, admin.site.urls),
-]
+] + DRF_SPECTACULAR_URLS
