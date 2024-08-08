95348a81fdb0ed4ce1306b5205027584e165c3a7
Fernando Toledo <fernandohtr@gmail.com>
2024-08-08 10:48:04 -0300

    style: fix too long line

--- src/v1/users/managers.py
+++ src/v1/users/managers.py
@@ -55,6 +55,8 @@ class CustomUserManager(BaseUserManager):
         else:
             raise ValueError(_("Superuser must have an email address."))
 
-        user = self.create_user(first_name=first_name, last_name=last_name, email=email, password=password, **extra_fields)
+        user = self.create_user(
+            first_name=first_name, last_name=last_name, email=email, password=password, **extra_fields
+        )
         user.save(using=self._db)
         return user
