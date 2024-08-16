ae34f52fd4d1b189125f32d1c3a4aed8c9977fbe
Fernando Toledo <fernandohtr@gmail.com>
2024-08-16 17:17:49 -0300

    feat: create ArticleAdmin and ArticleViewAdmin

--- src/v1/articles/admin.py
+++ src/v1/articles/admin.py
@@ -1,1 +1,51 @@
-# Register your models here.
+from django.contrib import admin
+
+from v1.articles.models import Article, ArticleView
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = [
+        "pkid",
+        "author",
+        "title",
+        "slug",
+        "view_count",
+    ]
+    list_display_links = [
+        "pkid",
+        "author",
+    ]
+    list_filter = [
+        "created_at",
+        "updated_at",
+    ]
+    search_fields = [
+        "title",
+        "body",
+        "tags",
+    ]
+    ordering = ["-created_at"]
+
+
+@admin.register(ArticleView)
+class ArticleViewAdmin(admin.ModelAdmin):
+    list_display = [
+        "pkid",
+        "article",
+        "user",
+        "viewer_ip",
+    ]
+    list_display_links = [
+        "pkid",
+        "article",
+    ]
+    list_filter = [
+        "created_at",
+        "updated_at",
+    ]
+    search_fields = [
+        "article",
+        "user",
+        "viewer_ip",
+    ]
