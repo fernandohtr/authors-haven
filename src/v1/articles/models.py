5e863bb87fb4ba40d8f7f8b9a94747da47c32142
Fernando Toledo <fernandohtr@gmail.com>
2024-08-16 17:06:35 -0300

    feat: create Article and ArticleView models

--- src/v1/articles/models.py
+++ src/v1/articles/models.py
@@ -1,1 +1,52 @@
-# Create your models here.
+from autoslug import AutoSlugField
+from django.contrib.auth import get_user_model
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from taggit.managers import TaggableManager
+
+from v1.articles.read_time_engine import ArticleReadTimeEngine
+from v1.common.models import TimeStampedModel
+
+User = get_user_model()
+
+
+class Article(TimeStampedModel):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="articles")
+    title = models.CharField(verbose_name=_("Title"), max_length=255)
+    slug = AutoSlugField(populate_from="title", always_update=True, unique=True)
+    description = models.CharField(verbose_name=_("Description"), max_length=255)
+    body = models.TextField(verbose_name=_("article content"))
+    banner_image = models.ImageField(verbose_name=_("banner image"), default="/prodile_default.png")
+    tags = TaggableManager()
+
+    def __str__(self):
+        return f"{self.author.first_name}'s article"
+
+    @property
+    def estimated_reading_time(self):
+        return ArticleReadTimeEngine.estimate_reading_time(self)
+
+    def view_count(self):
+        return self.article_views.count()
+
+
+class ArticleView(TimeStampedModel):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name="article_views")
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="user_views")
+    viewer_ip = models.GenericIPAddressField(verbose_name=_("viewer IP"), null=True, blank=True)
+
+    class Meta:
+        verbose_name = _("Article View")
+        verbose_name_plural = _("Article Views")
+        unique_together = ("article", "user", "viewer_ip")
+
+    def __str__(self):
+        return (
+            f"{self.article.title} viwed by "
+            f"{self.user.first_name if self.user else 'Anonymous'} from IP {self.viewer_ip}"
+        )
+
+    @classmethod
+    def record_view(cls, article, user, viewer_ip):
+        view, _ = cls.objects.get_or_create(article=article, user=user, viewer_ip=viewer_ip)
+        view.save()
