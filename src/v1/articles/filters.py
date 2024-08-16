3e495f6dc23dfa0fbf8d35fb9f1ecaf01d267437
Fernando Toledo <fernandohtr@gmail.com>
2024-08-16 17:26:16 -0300

    feat: create ArticleFilter

---
+++ src/v1/articles/filters.py
@@ -0,0 +1,20 @@
+import django_filters
+from v1.articles.models import Article
+
+class ArticleFilter(django_filters.FilterSet):
+    author = django_filters.CharFilter(field_name="author__first_name", lookup_expr="icontains")
+    title = django_filters.CharFilter(field_name="title", lookup_expr="icontains")
+    tags = django_filters.CharFilter(field_name="tags__name", lookup_expr="iexact")
+    created_at = django_filters.DateFromToRangeFilter(field_name="created_at")
+    updated_at = django_filters.DateFromToRangeFilter(field_name="updated_at")
+
+    class Meta:
+        model = Article
+        fields = [
+            "author",
+            "title",
+            "tags",
+            "created_at",
+            "updated_at",
+        ]
+    
