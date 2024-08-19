135dc3f77a19679c2ac41ffde485af3225c04745
Fernando Toledo <fernandohtr@gmail.com>
2024-08-19 16:40:17 -0300

    feat: create RatingAdmin

--- src/v1/ratings/admin.py
+++ src/v1/ratings/admin.py
@@ -1,1 +1,15 @@
-# Register your models here.
+from django.contrib import admin
+
+from v1.ratings.models import Rating
+
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "user",
+        "article",
+        "rating",
+        "created_at",
+        "updated_at",
+    ]
