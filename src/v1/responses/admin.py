41aedbba513b362dff416018838eacbce5e20532
Fernando Toledo <fernandohtr@gmail.com>
2024-08-20 14:15:28 -0300

    feat: create ResponseAdmin

--- src/v1/responses/admin.py
+++ src/v1/responses/admin.py
@@ -1,1 +1,21 @@
-# Register your models here.
+from django.contrib import admin
+
+from v1.responses.models import Response
+
+
+@admin.register(Response)
+class ResponseAdmin(admin.ModelAdmin):
+    list_display = [
+        "pkid",
+        "id",
+        "user",
+        "article",
+        "parent_response",
+        "content",
+        "created_at",
+    ]
+    list_display_links = [
+        "pkid",
+        "id",
+        "user",
+    ]
