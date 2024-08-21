40115d560819aefd86adfd38ccaa02c04dbabc5a
Fernando Toledo <fernandohtr@gmail.com>
2024-08-21 08:58:43 -0300

    feat: create search signals

--- src/v1/search/apps.py
+++ src/v1/search/apps.py
@@ -6,3 +6,7 @@ class SearchConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "v1.search"
     verbose_name = _("Search")
+
+    @staticmethod
+    def ready():
+        from v1.search import signals  # noqa
