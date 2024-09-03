57f1fca2a47a8f9d19e39a45340bfda0a1740158
Fernando Toledo <fernandohtr@gmail.com>
2024-09-03 08:38:40 -0300

    chore: create mock_request

--- conftest.py
+++ conftest.py
@@ -1,4 +1,7 @@
 import pytest
+from django.contrib.auth.middleware import AuthenticationMiddleware
+from django.contrib.sessions.middleware import SessionMiddleware
+from django.test import RequestFactory
 from pytest_factoryboy import register
 from v1.users.tests.factories import UserFactory
 
@@ -15,3 +18,17 @@ def normal_user(db, user_factory):
 def super_user(db, user_factory):
     new_user = user_factory.create(is_superuser=True, is_staff=True)
     return new_user
+
+
+@pytest.fixture
+def mock_request():
+    factory = RequestFactory()
+    request = factory.get("/")
+    middleware = SessionMiddleware(lambda req: None)
+    middleware.process_request(request)
+    request.session.save()
+
+    auth_middleware = AuthenticationMiddleware(lambda req: None)
+    auth_middleware.process_request(request)
+
+    return request
