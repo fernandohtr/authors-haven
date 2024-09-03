e3b523ebb56c604ef4d8ab6c5968aa537aea35ea
Fernando Toledo <fernandohtr@gmail.com>
2024-09-03 11:22:58 -0300

    chore: create a function to check celery

--- docker/local/django/celery/flower/start-flower.sh
+++ docker/local/django/celery/flower/start-flower.sh
@@ -4,6 +4,17 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
+worker_ready() {
+    celery -A config.celery inspect ping
+}
+
+until worker_ready; do
+    >&2 echo "Celery workers not available :-("
+    sleep 1
+done
+
+>&2 echo "Celery workers are available and ready!... :-)"
+
 exec watchfiles celery.__main__.main \
     --args \
     "-A config.celery -b '${CELERY_BROKER}' flower --basic_auth=${CELERY_FLOWER_USER}:${CELERY_FLOWER_PASSWORD} --broker_api=${CELERY_BROKER_API}"
